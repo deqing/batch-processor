ef55eba4a622d25a0714b69f8c48297bcb8c729e
Devin Huang <hdqdevin@gmail.com>
2024-04-14 16:21:29 +1000

    basic BatchProcessor for testing

---
+++ batchprocessor.go
@@ -0,0 +1,30 @@
+package batchprocessor
+
+import (
+	"fmt"
+	. "github.com/deqing/batching-api/api"
+	"log"
+	"time"
+)
+
+type BatchProcessor struct{}
+
+// Process simply changes all job status to SUBMITTED and logs the jobs that are processed
+func (b *BatchProcessor) Process(jobs []Job) {
+	for i := range jobs {
+		jobs[i].Status = SUBMITTED
+	}
+
+	var output string
+	for _, job := range jobs {
+		output = output + fmt.Sprintf("[%s %s \"%s\"] ", job.Id.String()[:8], job.Type, job.Name)
+	}
+	log.Printf("Processed jobs: %s", output)
+}
+
+// ProcessAndSleep is a wrapper function that calls Process and then sleeps for a given duration
+func (b *BatchProcessor) ProcessAndSleep(jobs []Job, seconds int) {
+	b.Process(jobs)
+	log.Printf("Sleeping for %d seconds", seconds)
+	time.Sleep(time.Duration(seconds) * time.Second)
+}
